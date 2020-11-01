f37d2f1c248ab89075978c614ee9ed3f4b885419
BBaoVanC <github@bbaovanc.com>
2020-10-31 21:51:38 -0500

    Remove acceleration (move to xorg config)

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 xset r rate 300 50
-xinput --set-prop 15 'libinput Accel Profile Enabled' 0, 1
-feh --bg-fill ~/Pictures/wallpaper/Color_Burst_2.jpg
+feh --bg-fill ~/wallpaper
 picom -b
 slstatus &

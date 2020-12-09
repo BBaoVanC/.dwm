0e96f810993a629fb5231a2074da7c63a4d7fd54
BBaoVanC <github@bbaovanc.com>
2020-12-09 17:22:06 -0600

    Temporarily disable light locker

--- autostart.sh
+++ autostart.sh
@@ -3,7 +3,8 @@ xset r rate 300 50
 feh --no-fehbg --bg-fill ~/wallpaper
 
 picom -b
-light-locker &
+# light locker currently causes xorg to core dump after unlocking
+#light-locker &
 goblocks &
 
 echo UPDATESTARTUPTTY | gpg-connect-agent &

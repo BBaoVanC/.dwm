f04e06ee6747c1f662d2258e4de16a119226158f
BBaoVanC <github@bbaovanc.com>
2020-12-22 18:49:21 -0600

    Add comments to autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-xset r rate 300 50
-feh --no-fehbg --bg-fill ~/wallpaper
+xset r rate 300 50  # keyboard repeat speed
+feh --no-fehbg --bg-fill ~/wallpaper  # set background
 
-nm-applet &
-picom -b
-light-locker &
-dwmblocks &
-pulse_daemon.sh &
+nm-applet &  # networkmanager tray icon
+picom -b  # compositor (daemonized)
+light-locker &  # enable lightdm locking
+dwmblocks &  # start dwmblocks
+pulse_daemon.sh &  # update dwmblocks when pulseaudio volume changes
 
 echo UPDATESTARTUPTTY | gpg-connect-agent &
